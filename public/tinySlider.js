b5a88759e7eb3eb412a28b9d3e17bf42a5e82ec9
Lorena Infante <m.lorenainfanter@gmail.com>
2023-12-25 20:08:04 -0300

    adjusting tinyslider in index.html

--- public/tinySlider.js
+++ public/tinySlider.js
@@ -1,7 +1,37 @@
 var slider = tns({
-    "container": ".my-slider",
-    "items": 3,
-    "gutter": 10,
-    "swipeAngle": false,
-    "speed": 400
-  });
+  container: ".my-slider",
+  items: 3,
+  center: true,
+  mouseDrag: true,
+  gutter: 3,
+  loop: true,
+  speed: 400,
+  responsive: {
+    375: {
+      items: 1.5,
+      center: true,
+      gutter: 1,
+      speed: 300,
+    },
+    576: {
+        items: 2,
+        gutter: 0,
+        speed: 300,
+      },
+    768: {
+      items: 3,
+      gutter: 1,
+      viewportMax: 100,
+    },
+    992: {
+      items: 4,
+      gutter: 0,
+      viewportMax: 100,
+    },
+    1200: {
+      items: 5,
+      gutter: 3,
+      viewportMax: 100,
+    },
+  },
+});
